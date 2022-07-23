99fea065f9b5ca10f3689aa25da39648d44f8090
Mohammed Musaib Ali <64194019+mdmusaibali@users.noreply.github.com>
2022-07-23 11:55:07 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+//122: DOM Element Selection & OOP Rendering
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedHTMLContent = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedHTMLContent.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
